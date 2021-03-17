49334b9d2e7dd82cb79e532019cba16aa6813473
XiaoRong <cjohnxiao@gmail.com>
2021-03-17 17:21:19 +0800

    这个版本不错

--- QT/LocalScaleWidget/main.cpp
+++ QT/LocalScaleWidget/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    LocalScaleWidget localScaleWidget;
+    ImageView localScaleWidget;
     localScaleWidget.show();
     return a.exec();
 }
--- QT/LocalScaleWidget/widget.h
+++ QT/LocalScaleWidget/widget.h
@@ -1,26 +1,39 @@
-﻿#ifndef WIDGET_H
-#define WIDGET_H
-
+﻿#ifndef IMAGE_VIEW_H
+#define IMAGE_VIEW_H
 #include <QWidget>
 #include <QImage>
-#include <QPaintEvent>
 
-
-class LocalScaleWidget : public QWidget
+class ImageView : public QWidget
 {
-     Q_OBJECT
-     public:
-          LocalScaleWidget(QWidget *parent=0);
-          ~LocalScaleWidget();
-     public:
-          void paintEvent(QPaintEvent *e);
-          void mouseMoveEvent(QMouseEvent *event);
-     private:
-          int x,y;
-          int width,height;
-          QPixmap pixmap;
-          QPixmap mypixmap ;
-          QPixmap background;
+    Q_OBJECT
+
+public:
+    ImageView(QWidget *parent = nullptr);
+    ~ImageView();
+
+protected:
+    void contextMenuEvent(QContextMenuEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private:
+    QImage m_Image;
+    qreal m_ZoomValue = 1.0;
+    int m_XPtInterval = 0;
+    int m_YPtInterval = 0;
+    QPoint m_OldPos;
+    bool m_Pressed = false;
+
+private slots:
+    void onLoadImage(void);
+    void onZoomInImage(void);
+    void onZoomOutImage(void);
+    void onPresetImage(void);
 };
 
-#endif // WIDGET_H
+
+#endif
+
